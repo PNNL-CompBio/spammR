f76877c79f2c343c157c926b41ed93ccf0a98791
HarkiratSohi <harkirat.sohi@pnnl.gov>
2023-07-11 12:44:49 -0700

    Volcano plots for spatialDiffEx results
    See issue # 5 under Issues

---
+++ R/volcanoPlot_DiffExResults.R
@@ -0,0 +1,4 @@
+#' Generate a volcano plot to visualize results from differential expression analysis.
+#' In spammR, these could be results obtained from the spatialDiffEx() function.
+#' @export
+#' @param spe: SpatialExperiment object containing results from differential expression
