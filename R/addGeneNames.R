88a64587ef37ff52bccc9d24e872417bce315c60
HarkiratSohi <harkirat.sohi@pnnl.gov>
2023-07-13 19:27:30 -0700

    Create addGeneNames.R

---
+++ R/addGeneNames.R
@@ -0,0 +1,9 @@
+#' Obtain gene names for each feature (currently, protein) in a given dataset in an spe object, add a column with gene names to the dataset and return a new spe object
+#' Run this function before running any geneset based analysis functions if the starting data doesn't have a gene name column already.
+#' Gene names are needed for any pathway/geneset based analysis as the members in each geneset are given as gene names, not protein identifiers.
+#' @export
+#' @param spe Spatial Experiment object containing the omics dataset
+#' @param featureID_colname Column name containing feature names for which gene names are needed (Example: "Protein_rowID")
+#' @param species Name of the species whose data is in spe (example: "human")
+#' @param db_file Path or name of database file containing information for converting from feature ID to gene name. Defaults to database files provided in spammR for the given species
+#' @return spe_withGeneNames Spatial Experiment object containing everything in the input spe, along with a column for gene name.
