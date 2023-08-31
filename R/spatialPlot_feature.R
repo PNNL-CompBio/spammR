5e3b57448e93af04d9ac3d899446d058faa8234e
HarkiratSohi <harkirat.sohi@pnnl.gov>
2023-08-31 16:10:11 -0700

    Added spatial_coord_type as an input parameter to allow for flexibility in plotting based on the type of position (topleft_corner, topright_corner) of the coordinates for samples.

--- R/spatialPlot_feature.R
+++ R/spatialPlot_feature.R
@@ -5,11 +5,13 @@
 # Assumes that colData(spe) and spatialCoords(spe) are provided in the same order of samples (rows).
 # Default values are specified for function paramters 'metric_display', 'label_column' and 'interactive,' if the user doesn't specify those.
 #' @param spe: SpatialExperiment object
+#' @param spatial_coord_type Position type for the given spatial coordinates of samples in spe. Current options are: "topleft_corner", "topright_corner"
+#' Future versions of spatialPlot_feature() to include additional options for spatial_coord_type: "center", bottomleft_corner", "bottomright_corner"
 #' @param feature: Name of the feature in the spe object whose values are to plotted in the spatial heat map. This should be a row name in rowData(spe)
 #' @param metric_display: Legend title for spatial heatmap. If this parameter is not specified, legend title defaults to "Protein abundance measure"
 #' @param label_column: Colunm in colData(spe) to be used for labeling grid squares. If not specified, default is no labels.
 #' @param interactive: Boolean value (TRUE/FALSE) indicating whether the plot should have interactive mouse hovering. If not specified, this defaults to TRUE.
-#' @return spatial_plot: Saptail heatmap of the chosen feature
+#' @return spatial_plot: Spatial heatmap of the chosen feature
 #  Note: grid squares can only be labeled when interactive = FALSE due to current ggplotly limitations.
 spatialPlot_feature<-function(spe,feature,metric_display = "Protein abundance measure",label_column=NA,interactive=TRUE){
   library(ggplot2)
@@ -31,10 +33,27 @@ spatialPlot_feature<-function(spe,feature,metric_display = "Protein abundance me
   }
   # Switched to using geom_rect instead of goem_raster because geom_raster positioning gets distorted when the plot is made interactive.
   # If not doing interactive hovering, then geom_raster may be more favorable to use.
-  p<- ggplot(spatial, aes(xmin = x-1, xmax = x, ymin = y-1, ymax = y, fill=feature_values_toplot, label = lab))+
+  x_left = c()
+  x_right = c()
+  y_botton = c()
+  y_top = c()
+  if (spatial_coord_type == "topright_corner"){
+    x_left = x-1
+    x_right = x
+    y_bottom = y-1
+    y_top = y
+  }else if (spatial_coord_type == "topleft_corner"){
+    x_left = x
+    x_right = x + 1
+    y_bottom = y-1
+    y_top = y 
+  }
+  midpoint_x = (x_left + x_right)/2
+  midpoint_y = (y_bottom + y_top)/2
+  p<- ggplot(spatial, aes(xmin = x_left, xmax = x_right, ymin = y_bottom, ymax = y_top, fill=feature_values_toplot, label = lab))+
     geom_rect()+
     scale_fill_viridis_c()+
-    geom_label(aes(x=(x+x-1)/2,y=(y+y-1)/2),label.size = NA, fill=NA)+
+    geom_label(aes(x=midpoint_x,y=midpoint_y),label.size = NA, fill=NA)+
     labs(fill = metric_display)+
     theme_bw()+
     xlim(0,NA)+
