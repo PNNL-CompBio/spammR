1b824be315f3d6eb2e786596d94a465245905e53
HarkiratSohi <116124471+HarkiratSohi@users.noreply.github.com>
2024-01-23 18:16:33 -0800

    Update volcanoPlot_DiffExSpe.R
    Added @import statement

--- R/volcanoPlot_DiffExSpe.R
+++ R/volcanoPlot_DiffExSpe.R
@@ -1,6 +1,7 @@
 #' Generate a volcano plot to visualize results from differential expression analysis.
 #' In spammR, these could be results obtained from the spatialDiffEx() function.
 #' log10(p-value) vs. log2(fold change)
+#' @import ggplot2
 #' @export
 #' @param spe SpatialExperiment object containing results from differential expression
 #' @param logFC_col column name in differenital expression results that represents log10 fold change
