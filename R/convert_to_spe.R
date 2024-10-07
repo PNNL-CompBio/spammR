121ab8d25b91431cf682634b69e4e7b07009bb31
HarkiratSohi <116124471+HarkiratSohi@users.noreply.github.com>
2024-10-07 16:14:32 -0700

    Fixed documentation and typos

--- R/convert_to_spe.R
+++ R/convert_to_spe.R
@@ -16,51 +16,55 @@
 #' @returns spe.out a SpatialExperiment (SPE) object that contains all data and image(s). Ready to be used as input in spammR functions that require SPE object as input.
 
 # Example input parameters
-# omics_measurements_file = "/Users/sohi472/Library/CloudStorage/OneDrive-PNNL/Projects/BICCN/data/brain_14ROIs_data_Aug24_2023/useThis/image0_data_only/image0_prot_dat_shortSampleNames.xlsx"
-# assay_name = "abundance" # Name to be given to the current data.
-# metadata_file = "/Users/sohi472/Library/CloudStorage/OneDrive-PNNL/Projects/BICCN/data/brain_14ROIs_data_Aug24_2023/useThis/image0_data_only/image0_samples_meta_data.xlsx"
-# meta_colname_sampleIDs = "Sample_ID"
-# remove_samples = c("0_TH_3","0_TEP_3","0_TH_2")
-# feature_colname = "PG.ProteinNames"
-# spatialCoords_colnames = c("Xcoord","Ycoord")
-# samples_common_identifier = "Image0" # Name of a common identifier for samples
+# omics_measurements_file1 = "/Users/sohi472/Library/CloudStorage/OneDrive-PNNL/Projects/BICCN/data/brain_14ROIs_data_Aug24_2023/useThis/image0_data_only/image0_prot_dat_shortSampleNames.xlsx"
+# assay_name1 = "abundance" # Name to be given to the current data.
+# metadata_file1 = "/Users/sohi472/Library/CloudStorage/OneDrive-PNNL/Projects/BICCN/data/brain_14ROIs_data_Aug24_2023/useThis/image0_data_only/image0_samples_meta_data.xlsx"
+# meta_colname_sampleIDs1 = "Sample_ID"
+# remove_samples1 = c("0_TH_3","0_TEP_3","0_TH_2")
+# feature_colname1 = "PG.ProteinNames"
+# spatialCoords_colnames1 = c("Xcoord","Ycoord")
+# samples_common_identifier1 = "Image0" # Name of a common identifier for samples
 # img1 = "/Users/sohi472/Library/CloudStorage/OneDrive-PNNL/Projects/BICCN/data/brain_14ROIs_data_Aug24_2023/tissue_images_forPlotting/Image0_Raw_noMarkings_cropped_forPlotting.png"
 # img2 = "/Users/sohi472/Library/CloudStorage/OneDrive-PNNL/Projects/BICCN/data/brain_14ROIs_data_Aug24_2023/tissue_images_forPlotting/Image0_ROIsMarked_cropped_forPlotting.png"
-# image_files = c(img1,img2)
-# image_ids = c("Raw_noMarkings","ROIsMarked") # Image names/identifiers for image paths provided in image_files
-# image_samples_common_identifier = c("Image0","Image0") #Name of a common identifier that links specific samples to a experiment/condition represented by a given image.
+# image_files1 = c(img1,img2)
+# image_ids1 = c("Raw_noMarkings","ROIsMarked") # Image names/identifiers for image paths provided in image_files
+# image_samples_common_identifier1 = c("Image0","Image0") #Name of a common identifier that links specific samples to a experiment/condition represented by a given image.
 
-convert_to_spe <-function(omics_measurements_file, metadata_file, meta_colname_sampleIDs, remove_samples, feature_colname, spatialCoords_colnames, samples_common_identifier, image_files, image_ids, image_samples_common_identifier){
-remove_sample_colnums = which(colnames(dat) %in% remove_samples)
-dat = dat[,-remove_sample_colnums]
-meta_dat = meta_dat[!meta_dat[,meta_colname_sampleIDs] %in% remove_samples,]
-# Separate sample columns and feature meta data columns in dat
-sample_colnums = which(colnames(dat) %in% meta_dat[,meta_colname_sampleIDs])
-sample_colnames = colnames(dat)[sample_colnums]
-dat_samples_only = dat[,sample_colnames]
-features_info = dat[,-c(sample_colnums)] # to be specified as rowData for SPE
-# The list of samples specified in the data and metadata to be stored in the SPE object should be exactly the same.
-# Keep rows in meta data that have a corresponding sample ID in the omics measurements file
-meta_dat_keep = meta_dat[meta_dat[,meta_colname_sampleIDs] %in% sample_colnames,] # To be specified as colData for SPE
-rownames(meta_dat_keep) = meta_dat[,meta_colname_sampleIDs]
-spe.out <-SpatialExperiment(assays=list(as.matrix(dat_samples_only)),
-                            colData=meta_dat_keep,
-                            rowData=features_info,
-                            spatialCoords = as.matrix(meta_dat_keep[,spatialCoords_colnames]),
-                            sample_id = samples_common_identifier)
-names(assays(spe.out)) = c(assay_name)
-# Add image(s) to SPE
-if (!is.null(image_files)){
-  for (i in 1:length(image_files)){
-    # scaleFactor under addImg() is a "single numeric scale factor used to rescale spatial coordinates according to the image's resolution."
-    # Can turn scaleFactor into a parameter specified by user also, but not needed right now.
-    spe.out = addImg(spe.out,
-                     sample_id = image_samples_common_identifier[i],
-                     image_id = image_ids[i],
-                     imageSource = image_files[i],
-                     scaleFactor = NA_real_,
-                     load = TRUE)
+convert_to_spe <-function(omics_measurements_file, assay_name, metadata_file, meta_colname_sampleIDs, remove_samples=NA, feature_colname, spatialCoords_colnames, samples_common_identifier, image_files, image_ids, image_samples_common_identifier){
+  dat = data.frame(read_excel(path=omics_measurements_file),check.names = FALSE)
+  meta_dat = data.frame(read_excel(path=metadata_file),check.names = FALSE)
+  if(!is.na(remove_samples)){
+    remove_sample_colnums = which(colnames(dat) %in% remove_samples)
+    dat = dat[,-remove_sample_colnums]
+    meta_dat = meta_dat[!meta_dat[,meta_colname_sampleIDs] %in% remove_samples,]
   }
-}
-return(spe.out)
+  # Separate sample columns and feature meta data columns in dat
+  sample_colnums = which(colnames(dat) %in% meta_dat[,meta_colname_sampleIDs])
+  sample_colnames = colnames(dat)[sample_colnums]
+  dat_samples_only = dat[,sample_colnames]
+  features_info = dat[,-c(sample_colnums)] # to be specified as rowData for SPE
+  # The list of samples specified in the data and metadata to be stored in the SPE object should be exactly the same.
+  # Keep rows in meta data that have a corresponding sample ID in the omics measurements file
+  meta_dat_keep = meta_dat[meta_dat[,meta_colname_sampleIDs] %in% sample_colnames,] # To be specified as colData for SPE
+  rownames(meta_dat_keep) = meta_dat[,meta_colname_sampleIDs]
+  spe.out <-SpatialExperiment(assays=list(as.matrix(dat_samples_only)),
+                              colData=meta_dat_keep,
+                              rowData=features_info,
+                              spatialCoords = as.matrix(meta_dat_keep[,spatialCoords_colnames]),
+                              sample_id = samples_common_identifier)
+  names(assays(spe.out)) = c(assay_name)
+  # Add image(s) to SPE
+  if (!is.null(image_files)){
+    for (i in 1:length(image_files)){
+      # scaleFactor under addImg() is a "single numeric scale factor used to rescale spatial coordinates according to the image's resolution."
+      # Can turn scaleFactor into a parameter specified by user also, but not needed right now.
+      spe.out = addImg(spe.out,
+                       sample_id = image_samples_common_identifier[i],
+                       image_id = image_ids[i],
+                       imageSource = image_files[i],
+                       scaleFactor = NA_real_,
+                       load = TRUE)
+    }
+  }
+  return(spe.out)
 }
