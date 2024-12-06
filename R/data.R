f00d072ef64704b35166ec55e9661d05237d956f
Sara JC Gosline <sara.gosline@pnnl.gov>
2024-12-06 15:49:02 -0800

    Added in data documentation.

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Here is the data to be included in the spammR package
+#'
+#'
+#' Pancreatic proteomics measurements from Gosline et al. proteomics study.
+#' Row names indicate individual proteins, columns indicate sample identifiers described in
+#' `pancMeta`. Individual values show un-transformed abundance values for each protein.
+#' @references \url{data_blah.com}
+"pancData"
+
+#'
+#' Sample metadata for the `pancData` file.
+#'
+"pancMeta"
